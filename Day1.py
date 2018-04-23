81d0028a83ba59009c0a3838eb4dd191ec23969e
Idarlene Araujo <37625274+idarlenearaujo@users.noreply.github.com>
2018-04-23 08:51:31 -0300

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,17 @@
+# variables
+numberInt = 0
+numberDoube = 0.0
+fraseString = ''
+
+# new values
+numberInt = int(input())
+numberDoube = float(input())
+fraseString = str(input())
+
+# sum
+soma = i + numberInt
+somaF = d + numberDoube
+conc = s + fraseString
+
+# print
+print('{}\n{:.1f}\n{}'.format(soma, somaF, conc))
