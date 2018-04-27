ed68159138cf3d85b062bf91f9ec81a9a76a54ba
Idarlene Araujo <37625274+idarlenearaujo@users.noreply.github.com>
2018-04-27 14:33:02 -0300

    Add files via upload

---
+++ Day7.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+import sys
+
+# variaveis
+lista = []
+
+# numero de numeros no array
+# array
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+
+# inverter posicoes
+arr.reverse()
+
+# mostrar na tela resposta
+for i in range(n):
+    print(arr[i], end=" ")
